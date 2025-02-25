f3fb8f0f66cb635c43d8670a07a418c19ea1bd96
16gsanket <16gsanket@gmail.com>
2025-02-25 15:26:50 +0530

    connected the database to the next app post authenitcation to contain the KEY

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { auth } from '../_lib/auth'
 export const metadata = {
   title:"Account"
 }
 
-function page() {
+
+async function page() {
+  const session = await auth();
+  console.log(session);
+  
   return (
     <section>
         <h2 className='text-2xl text-white'>This is Account Page</h2>
